018f2caf4a7eceb91c864306eb5dd95e755693a9
GuerreroM05 <daimerguerrerofuratena@gmail.com>
2023-10-25 13:18:11 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+require("colors");
+
+const math = require("./modules/math.js")
+
+console.clear();
+console.log(math);
+
+const main = async() => {
+    console.log("***************************************".blue);
+    console.log("*     ".blue,"funciones matematicas".bgBlue,
+    "     *".blue);
+    console.log("***************************************\n".blue);
+    console.log("***************************************".red);
+    console.log("*                                      *".red);
+
+
+    console.log(math.add(5,3)); 
+    console.log(math.substract(2,8));
+    console.log(math.multiply(2,8));
+    console.log(math.divide(4,2));
+}
+
+main();
