3f1ee78f8dcf5c66793a4f29abb62dc70b1591cb
GuerreroM05 <daimerguerrerofuratena@gmail.com>
2023-10-29 20:05:29 -0500

    documentacion del codigo

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-require("colors");
+//importa el paquete 'colors'
+require ("colors");
 
 const math = require("./modules/math.js")
-
+// limpia la consola
 console.clear();
-console.log(math);
 
+console.log(math);
+//función asíncrona llamada 'main'.
 const main = async() => {
     console.log("***************************************".blue);
     console.log("*     ".blue,"funciones matematicas".bgBlue,
@@ -14,10 +16,10 @@ const main = async() => {
     console.log("*                                      *".red);
 
 
-    console.log(math.add(5,3)); 
-    console.log(math.substract(2,8));
-    console.log(math.multiply(2,8));
-    console.log(math.divide(4,2));
+    console.log(math.add(5,3)); //llama a la funcion add
+    console.log(math.substract(2,8));//llama a la funcion substract
+    console.log(math.multiply(2,8));//llama a la funcion multiply
+    console.log(math.divide(4,2));//llama a la funcion divide
 }
 
 main();
--- modules/math.js
+++ modules/math.js
@@ -3,22 +3,23 @@ const math = {};
 function add(a,b) {
     return a + b;
 }
-
+//Se define una función de suma que toma dos parámetros a y b y devuelve su suma.
 function substract(a,b) {
     return a - b;
 }
-
+// Se define una función de resta que toma dos parámetros a y b y devuelve su diferencia.
 function multiply(a,b){
     return a * b;
 }
-
+//Se define una función de multiplicación que toma dos parámetros a y b y devuelve su producto.
 function divide(a,b){
     if (b == 0){
         return "Error no se puede dividir entre cero";
     }else {
         return a / b;
     }
-    
+    // verifica si el divisor b es cero. 
+    //Si es cero, devuelve un mensaje de error; de lo contrario, devuelve el cociente de a dividido por b.
 }
 
 math.add = add;
@@ -27,3 +28,4 @@ math.multiply = multiply;
 math.divide = divide;
 
 module.exports = math;
+//Se exporta el objeto math para que esté disponible para su uso en otros archivos
